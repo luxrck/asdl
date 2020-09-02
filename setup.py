399747cebb5b4f9022202b9d6e1509018a822f5a
Aaron Lux <lux.r.ck@gmail.com>
2020-09-02 15:16:54 +0800

    initial commit.
    Add asdl.py from CPython3.9

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+__version__ = "0.1.0"
+
+setup(name='asdl',
+      version=__version__,
+      description='Python ASDL parser (from CPython3.X)',
+      author='lux.r.ck',
+      author_email='lux.r.ck@gmail.com',
+      py_modules=["asdl"],
+      license="MIT License",
+     )
